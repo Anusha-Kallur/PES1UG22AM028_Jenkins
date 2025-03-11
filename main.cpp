c9a1b0e6a18989fa8719912a638c49d32160a7f8
Anusha Kallur <143982318+Anusha-Kallur@users.noreply.github.com>
2025-03-11 16:49:09 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//File: main.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, Jenkins\n";
+  cout << "I am Anusha K  \n";
+  cout << "I have successfully built and run this in main.cpp  \n";
+  return 0;
+}
